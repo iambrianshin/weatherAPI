02ae1f331ba71ba3c0a6d836a41869448a7b0b55
Brian Shin <39751822+iambrianshin@users.noreply.github.com>
2018-12-25 21:27:44 -0500

    Update Titles.js

--- components/Titles.js
+++ components/Titles.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-class Titles extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Weater Fidner</h1>
-        <p> Find out temperature, conditions and more...</p>
-      </div>
-    );
-  }
-}
-
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weater Fidner</h1>
+    <p className="title-container__subtitle">
+      {" "}
+      Find out temperature, conditions and more...
+    </p>
+  </div>
+);
 export default Titles;
